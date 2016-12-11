0bf2e1d1c43cff2d6595cf62dcfc9366dfe8af3e
anamariaciuhan <anamaria.ciuhan@gmail.com>
2016-12-12 00:42:08 +0200

    Exercitii MVC Course 5

---
+++ ProjectManagement/App_Data/Models/AboutModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManagement.Models
+{
+    public class AboutModel // exercitiul 7 mvc course 5
+
+
+
+    { 
+
+        public string Message { get; set; }
+        public string Author { get; set; }
+
+
+    }
+}
---
+++ ProjectManagement/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ProjectManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Author = "Ciuhan Ana-Maria"; // exercitiul 6 mvc course 5
+
+            var model = new AboutModel(); // exercitiul 7 mvc course 5
+            model.Message = "Hello";
+            model.Author = "Ciuhan Ana Maria";
+            return View(model);
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
